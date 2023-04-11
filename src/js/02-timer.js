d30eb3dc13b6d9e09712a8cff98ec2d31720c848
Oleksii cherkasov <olexiyc@gmail.com>
2023-04-11 08:08:57 +0800

    T

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,76 +3,75 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 
-
-
-const inputId = document.querySelector('input#datatime-picker');
-const minuteId = document.querySelector('[data-minutes]');
-const secondsId = document.querySelector('[data-second]');
-const hoursId = document.querySelector('[data-hours]');
+const input = document.querySelector('input#datetime-picker');
+const btn = document.querySelector('button[data-start]');
 const daysId = document.querySelector('[data-days]');
-const btnId = document.querySelector('button[data-start]');
+const hoursId = document.querySelector('[data-hours]');
+const minutesId = document.querySelector('[data-minutes]');
+const secondsId = document.querySelector('[data-seconds]');
 
 let intervalsId = null;
-btnId.disabled = true;
+btn.disabled = true;
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      
-      if (selectedDates[0] <= new Date()) {
-          btnId.disabled = true;
-          Notiflix.Notify.failure("Please choose a date in the future");
-          return;
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+    console.log(selectedDates[0]);
+
+    if(selectedDates[0] <= new Date()) {
+        btn.disabled = true;
+        Notiflix.Notify.failure("Please choose a date in the future");
+        return;
       } else {
-          btnId.disabled = false;
+        btn.disabled = false;
       }
 
-      btnId.addEventListener('click', () => {
-          intervalsId = setInterval(() => {
-              const timeDifferences = selectedDates[0] - new Date();
+    btn.addEventListener('click', () => {
+         intervalsId = setInterval(() => {
+            const differenceInTime = selectedDates[0] - new Date();
 
-              if (timeDifferences < 1000) {
-                  clearInterval(intervalsId);
-              }
-              const result = convertMs(timeDifferences);
-              viewOfTimer(result);
-          })
+            if(differenceInTime < 1000) {
+                clearInterval(intervalsId);
+            }
+            const result = convertMs(differenceInTime);
+            viewOfTimer(result);
+         })
       })
-       
-  },
-};
 
-flatpickr(inputId, options);
+    },
+  };
+
+flatpickr(input, options);
 
 function viewOfTimer({ days, hours, minutes, seconds }) {
     daysId.textContent = `${days}`;
     hoursId.textContent = `${hours}`;
-    minuteId.textContent = `${minutes}`;
+    minutesId.textContent = `${minutes}`;
     secondsId.textContent = `${seconds}`;
 }
 
-function addZero(value) {
+function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 }
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = addZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addZero(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
-}
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+  }
+  
